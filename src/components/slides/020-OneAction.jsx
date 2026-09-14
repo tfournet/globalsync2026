23bcfdac8aefe5bb473da2dab1f93e44d1ac5c92
Tim Fournet <timfournet@gmail.com>
2026-09-14 17:21:21 -0500

    Slide 19: third question, threatened or relieved

--- src/components/slides/020-OneAction.jsx
+++ src/components/slides/020-OneAction.jsx
@@ -4,7 +4,7 @@ export default function OneAction() {
   return (
     <ClosingSlide
       title="Sit with one employee you would hate to lose"
-      action="Ask them what they do at least once a month that they dread. Work out the unwritten process behind it and write it down. Then ask two questions. Could someone else follow it without asking for help? Could some or all of it be automated?"
+      action="Ask them what they do at least once a month that they dread. Work out the unwritten process behind it and write it down. Then ask three questions. Could someone else follow it without asking for help? Could some or all of it be automated? Would that person feel threatened by that, or relieved?"
       ecosystemLine="Global Sync Meetups | Accelerators | Workshops"
       copyrightLine="Copyright Rocket Fuel Factory. All rights reserved."
     />
